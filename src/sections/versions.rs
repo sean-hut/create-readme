df469bb8fd5b3a00cfa35c74b656a37654db6fc9
Sean Hutchings <seanhut@yandex.com>
2020-11-05 21:36:07 -0500

    AA add verisons section
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

---
+++ src/sections/versions.rs
@@ -0,0 +1,15 @@
+pub fn development_version() -> &'static str {
+    "## Development Version
+
+The development version is at the head of the `develop` branch."
+}
+
+pub fn stable_version() -> &'static str {
+    "## Stable Versions
+
+Stable releases are tagged on the `releases` branch.
+
+The [SemVar][semvar] version of semantic versioning is used.
+
+[semvar]: <https://web.archive.org/web/20201009135328/https://semver.org/>"
+}
