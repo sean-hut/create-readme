4b751c20650da4412d6b5af3c6258f6984090046
Sean Hutchings <seanhut@yandex.com>
2020-11-09 23:59:00 -0500

    AA add command line general information
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,8 @@ use side_effects::create_readme;
 
 fn main() {
     create_readme();
+    let arguments = App::new("create-readme")
+        .version("0.1.0")
+        .author("Sean Hutchings <seanhut@yandex.com>")
+        .about("Create a readme markdown file")
 }
