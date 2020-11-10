7f65ea36250a052ef50d95bb5d43636746eff4cc
Sean Hutchings <seanhut@yandex.com>
2020-11-09 23:57:20 -0500

    AA add mit0 license
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

--- src/sections/license.rs
+++ src/sections/license.rs
@@ -15,7 +15,13 @@ Links for more information on the license:
 [0bsd-commentary]: <https://web.archive.org/web/20200909121328/https://landley.net/toybox/license.html>
 [0bsd-osi]: <https://web.archive.org/web/20200923194052/https://opensource.org/licenses/0BSD>";
 
+pub const MIT0: &str = "This project is licensed under the MIT No Attribution license (MIT-0).
 
 Links for more information on the license:
 
+- [MIT-0 License Commentary][mit0-commentary]
+- [Open Source Initiative: MIT-0][mit0-osi]
+
+[mit0-commentary]: <https://web.archive.org/web/20200920130347/https://romanrm.net/mit-zero>
+[mit0-osi]: <https://web.archive.org/web/20200923194159/https://opensource.org/licenses/MIT-0>";
 
