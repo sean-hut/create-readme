96c632ac58708b5311efb9c28080a3b235c14762
Sean Hutchings <seanhut@yandex.com>
2020-11-09 23:59:27 -0500

    AA add top heading option
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,15 @@ fn main() {
         .version("0.1.0")
         .author("Sean Hutchings <seanhut@yandex.com>")
         .about("Create a readme markdown file")
+        .arg(
+            Arg::with_name("top-heading")
+                .long("top-heading")
+                .short("t")
+                .value_name("TEXT")
+                .required(true)
+                .takes_value(true)
+                .multiple(false)
+                .display_order(1)
+                .help("Top heading text that follows \"# \""),
+        )
 }
