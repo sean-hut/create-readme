4702d48d59b5b0ef7566c68739b155a0940ac5f9
Sean Hutchings <seanhut@yandex.com>
2020-11-09 23:56:54 -0500

    R change 0bsd function into const
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

--- src/sections/license.rs
+++ src/sections/license.rs
@@ -2,13 +2,20 @@ pub const LICENCE: &str = "## License
 
 The license file is `LICENSE`.
 
-This project is licensed under the BSD Zero Clause license (0BSD).
+";
+
+pub const BSD0: &str =
+    "This project is licensed under the BSD Zero Clause license (0BSD).
 
 Links for more information on the license:
 
-- [License Commentary][landley]
-- [Open Source Initiative][osi]
+- [0BSD License Commentary][0bsd-commentary]
+- [Open Source Initiative: 0BSD][0bsd-osi]
 
-[landley]: <https://web.archive.org/web/20200909121328/https://landley.net/toybox/license.html>
-[osi]: <https://web.archive.org/web/20200923194052/https://opensource.org/licenses/0BSD>"
-}
+[0bsd-commentary]: <https://web.archive.org/web/20200909121328/https://landley.net/toybox/license.html>
+[0bsd-osi]: <https://web.archive.org/web/20200923194052/https://opensource.org/licenses/0BSD>";
+
+
+Links for more information on the license:
+
+
